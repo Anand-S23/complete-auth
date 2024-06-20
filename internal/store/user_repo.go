df3d8382295fbdc672eb177225afeadf39741757
Anand-S23 <anandsroopsingh23@gmail.com>
2024-06-19 23:17:40 -0400

    feat: insert user profile into db when inserting user

--- internal/store/user_repo.go
+++ internal/store/user_repo.go
@@ -25,24 +25,80 @@ func NewPgUserRepo(db *sql.DB) *PgUserRepo{
 }
 
 func (pg *PgUserRepo) InsertUser(ctx context.Context, user *models.User) error {
-    stmt, err := pg.Db.PrepareContext(ctx, `insert into users (id, email, password_hash) values ($1, $2, $3)`)
+    tx, err := pg.Db.BeginTx(ctx, nil)
     if err != nil {
         return err
     }
-    defer stmt.Close()
 
-    _, err = stmt.ExecContext(ctx, user.ID, user.Email, user.Password)
-    return err
+    defer func() {
+        if err != nil {
+            tx.Rollback()
+        } else {
+            err = tx.Commit()
+        }
+    }()
+
+    userInsertStatment, err := tx.PrepareContext(ctx, `insert into users (id, email, password_hash) values ($1, $2, $3)`)
+    if err != nil {
+        return err
+    }
+    defer userInsertStatment.Close()
+
+    _, err = userInsertStatment.ExecContext(ctx, user.ID, user.Email, user.Password)
+    if err != nil {
+        return err
+    }
+
+    profileInsertStatment, err := tx.PrepareContext(ctx, `insert into user_profiles (user_id, first_name, last_name, phone_number, pfp_url) values ($1, $2, $3, $4, $5)`)
+    if err != nil {
+        return err
+    }
+    defer profileInsertStatment.Close()
+
+    _, err = profileInsertStatment.ExecContext(ctx, user.ID, user.Email, user.Password)
+    if err != nil {
+        return err
+    }
+
+    return nil
 }
 
 func (pg *PgUserRepo) InsertOAuthUser(ctx context.Context, user *models.User) error {
-    stmt, err := pg.Db.PrepareContext(ctx, `insert into users (id, email, oauth_provider, oauth_id) values ($1, $2, $3, $4)`)
+    tx, err := pg.Db.BeginTx(ctx, nil)
     if err != nil {
         return err
     }
-    defer stmt.Close()
 
-    _, err = stmt.ExecContext(ctx, user.ID, user.Email, user.OAuthProvider, user.OAuthID)
-    return err
+    defer func() {
+        if err != nil {
+            tx.Rollback()
+        } else {
+            err = tx.Commit()
+        }
+    }()
+
+    userInsertStatment, err := tx.PrepareContext(ctx, `insert into users (id, email, oauth_provider, oauth_id) values ($1, $2, $3, $4)`)
+    if err != nil {
+        return err
+    }
+    defer userInsertStatment.Close()
+
+    _, err = userInsertStatment.ExecContext(ctx, user.ID, user.Email, user.OAuthProvider, user.OAuthID)
+    if err != nil {
+        return err
+    }
+
+    profileInsertStatment, err := tx.PrepareContext(ctx, `insert into user_profiles (user_id, first_name, last_name, phone_number, pfp_url) values ($1, $2, $3, $4, $5)`)
+    if err != nil {
+        return err
+    }
+    defer profileInsertStatment.Close()
+
+    _, err = profileInsertStatment.ExecContext(ctx, user.ID, user.Email, user.Password)
+    if err != nil {
+        return err
+    }
+
+    return nil
 }
 
